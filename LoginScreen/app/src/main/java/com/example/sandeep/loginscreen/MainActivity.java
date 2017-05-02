2d8a8498fd93d64a0676d93dfd08638482f3af54
Sandeep Hegde <sandeep.hegde123@gmail.com>
2017-05-02 20:44:31 +0530

    Implement the login screen.
    You need to implement the login screen having following fields
    
    Email Id/Name
    Password
    Log In/ Sign In

---
+++ LoginScreen/app/src/main/java/com/example/sandeep/loginscreen/MainActivity.java
@@ -0,0 +1,20 @@
+//Android Developer Training Session 2 - Assignment 2.3
+
+/*PROBLEM STATEMENT- You need to implement the login screen having following fields
+Email Id/Name
+Password
+Log In/ Sign In */
+
+package com.example.sandeep.loginscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
